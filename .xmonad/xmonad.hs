c624b028ecc0de4031027269222c3407853626d9
Guzman Alvarez <guzman@guzman.fi>
2016-11-28 09:39:36 +0200

    xmonad: clean up xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,7 +12,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import qualified Data.Map as M
 
 -- Workspaces
-myWorkspaces    = ["0","1","2","3","4","5","6","7","8","9"]
+myWorkspaces = ["0","1","2","3","4","5","6","7","8","9"]
 
 -- Borders
 myNormalBorderColor  = "#777777"
@@ -21,13 +21,15 @@ myFocusedBorderColor = "#ffffff"
 -- Keys
 myModMask = mod1Mask
 myKeys =
-    [ ((myModMask, xK_Up), sendMessage ShrinkSlave)
-    , ((myModMask, xK_Down), sendMessage ExpandSlave)
-    , ((myModMask .|. controlMask, xK_Page_Up), spawn "set_volume up")
-    , ((myModMask .|. controlMask, xK_Page_Down), spawn "set_volume down")
-    , ((myModMask .|. controlMask, xK_End), spawn "set_volume toggle")
-    , ((myModMask .|. controlMask, xK_l), spawn "gnome-screensaver-command --lock")
-    , ((shiftMask, 0xff61), spawn "sleep 0.2; scrot -s '%H_%M_%S__%d_%m_%y.png' -e 'mv $f ~/screenshots'")
+    [ ((myModMask, xK_Up),                          sendMessage ShrinkSlave)
+    , ((myModMask, xK_Down),                        sendMessage ExpandSlave)
+    , ((myModMask .|. controlMask, xK_Page_Up),     spawn "set_volume up")
+    , ((myModMask .|. controlMask, xK_Page_Down),   spawn "set_volume down")
+    , ((myModMask .|. controlMask, xK_End),         spawn "set_volume toggle")
+    , ((myModMask .|. controlMask, xK_l),
+        spawn "gnome-screensaver-command --lock")
+    , ((shiftMask, 0xff61),
+        spawn "sleep 0.2; scrot -s '%H_%M_%S__%d_%m_%y.png' -e 'mv $f ~/screenshots'")
     ]
     -- Switch to additional workspaces (other than 1-9, which is the default)
     ++
@@ -44,22 +46,22 @@ myLayoutHook = smartBorders $ mouseResizableTile { draggerType = BordersDragger 
 
 -- Specific window rules
 myManageHook = composeAll
-    [ isDialog                      --> doFloat
-    , className =? "MPlayer"        --> doFloat
-    , className =? "Gimp"           --> doFloat
-    , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore
-    , className =? "Thunderbird"    --> doShift "1"
-    , stringProperty "WM_NAME" =? "gist"                            --> doFloat
-    , stringProperty "WM_NAME" =? "notes"                           --> doFloat
-    , stringProperty "WM_NAME" =? "mixer"                           --> doFloat
-    , stringProperty "WM_NAME" =? "TTCN-3 Test Executor"            --> doFloat
-    , stringProperty "WM_NAME" =? "TTCN-3 Log Monitor"              --> doFloat
-    , stringProperty "WM_NAME" =? "Buddy List"                      --> doFloat
+    [ isDialog                                              --> doFloat
+    , className =? "MPlayer"                                --> doFloat
+    , className =? "Gimp"                                   --> doFloat
+    , resource  =? "desktop_window"                         --> doIgnore
+    , resource  =? "kdesktop"                               --> doIgnore
+    , className =? "Thunderbird"                            --> doShift "1"
+    , stringProperty "WM_NAME" =? "gist"                    --> doFloat
+    , stringProperty "WM_NAME" =? "notes"                   --> doFloat
+    , stringProperty "WM_NAME" =? "mixer"                   --> doFloat
+    , stringProperty "WM_NAME" =? "TTCN-3 Test Executor"    --> doFloat
+    , stringProperty "WM_NAME" =? "TTCN-3 Log Monitor"      --> doFloat
+    , stringProperty "WM_NAME" =? "Buddy List"              --> doFloat
 -- Thesis
-    , stringProperty "WM_NAME" =? "Plot area"                       --> doFloat
-    , stringProperty "WM_NAME" =? "Data table"                      --> doFloat
-    , stringProperty "WM_NAME" =? "Data selection"                  --> doFloat
+    , stringProperty "WM_NAME" =? "Plot area"               --> doFloat
+    , stringProperty "WM_NAME" =? "Data table"              --> doFloat
+    , stringProperty "WM_NAME" =? "Data selection"          --> doFloat
     , manageDocks <+> manageHook defaultConfig
     ]
 
@@ -67,9 +69,9 @@ myManageHook = composeAll
 -- Main
 main = do
     xmonad $ defaultConfig
-        { focusedBorderColor = myFocusedBorderColor
-        , layoutHook        = myLayoutHook
-        , manageHook        = myManageHook
-        , modMask           = myModMask
-        , workspaces        = myWorkspaces
-        , normalBorderColor  = myNormalBorderColor } `additionalKeys` myKeys
+        { focusedBorderColor    = myFocusedBorderColor
+        , layoutHook            = myLayoutHook
+        , manageHook            = myManageHook
+        , modMask               = myModMask
+        , workspaces            = myWorkspaces
+        , normalBorderColor     = myNormalBorderColor } `additionalKeys` myKeys
