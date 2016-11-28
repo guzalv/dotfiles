c8fe2d43b858b6766b12a02f2143a65ccf762c16
Guzman Alvarez <guzman@guzman.fi>
2016-11-28 09:22:48 +0200

    xmonad: support additional workspaces (and add workspace "0")

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,8 +5,12 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Grid
 import XMonad.Layout.Reflect
 import XMonad.Layout.MouseResizableTile
+import XMonad.StackSet hiding (workspaces)
 import XMonad.Util.EZConfig(additionalKeys)
 import qualified Data.Map as M
+
+-- Workspaces
+myWorkspaces    = ["0","1","2","3","4","5","6","7","8","9"]
 
 -- Borders
 myNormalBorderColor  = "#777777"
@@ -23,6 +27,11 @@ myKeys =
     , ((myModMask .|. controlMask, xK_l), spawn "gnome-screensaver-command --lock")
     , ((shiftMask, 0xff61), spawn "sleep 0.2; scrot -s '%H_%M_%S__%d_%m_%y.png' -e 'mv $f ~/screenshots'")
     ]
+    -- Switch to additional workspaces (other than 1-9, which is the default)
+    ++
+    [((m .|. myModMask, k), windows $ f i)
+    | (i, k) <- zip myWorkspaces [xK_0, xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9]
+    , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
 
 -- Layout
 myLayoutHook = smartBorders $ mouseResizableTile { draggerType = BordersDragger }
@@ -60,4 +69,5 @@ main = do
         , layoutHook        = myLayoutHook
         , manageHook        = myManageHook
         , modMask           = myModMask
+        , workspaces        = myWorkspaces
         , normalBorderColor  = myNormalBorderColor } `additionalKeys` myKeys
