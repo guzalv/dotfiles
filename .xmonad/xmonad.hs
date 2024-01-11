e581fa12c85c9774fb5d6c365f3955959ea3c9d8
Guzman Alvarez <guzman.alvarez@ericsson.com>
2024-01-11 10:12:49 +0100

    xmonad: Several updates to config file

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 -- Everythig here is copy-pasted from several sources, notably Miika's:
 -- https://github.com/nablaa/dotfiles/blob/master/.xmonad/xmonad.hs
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
@@ -21,17 +22,18 @@ myModMask = mod1Mask
 myKeys =
     [ ((myModMask, xK_Up),                          sendMessage ShrinkSlave)
     , ((myModMask, xK_Down),                        sendMessage ExpandSlave)
+    , ((myModMask, xK_Tab),                         moveTo Next NonEmptyWS)
     , ((myModMask .|. controlMask, xK_Page_Up),     spawn "set_volume up")
     , ((0, 0x1008ff13),                             spawn "set_volume up")
     , ((myModMask .|. controlMask, xK_Page_Down),   spawn "set_volume down")
     , ((0, 0x1008ff11),                             spawn "set_volume down")
     , ((myModMask .|. controlMask, xK_End),         spawn "set_volume toggle")
     , ((0, 0x1008ff12),                             spawn "set_volume toggle")
-    , ((myModMask .|. controlMask, xK_l),           spawn "lock_screen")
+    , ((myModMask .|. controlMask, xK_l),           spawn "slock")
     , ((myModMask .|. shiftMask, xK_s),             spawn "systemctl suspend")
     , ((0, 0x1008ff2f),                             spawn "systemctl suspend")
     , ((shiftMask, 0xff61),
-        spawn "sleep 0.2; scrot -s '%H_%M_%S__%d_%m_%y.png' -e 'mv $f ~/screenshots'")
+        spawn "sleep 0.2; scrot -s '%H_%M_%S__%d_%m_%y.png' -e 'mv $f ~/Pictures'")
     ]
 
 -- Layout
